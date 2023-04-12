11a7f4905c8c24b9888e0e356118e633db3878e0
shiitakedaisuki <129662461+shiitakedaisuki@users.noreply.github.com>
2023-04-12 14:02:39 +0000

    add2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,42 @@
 class BooksController < ApplicationController
-  def new
-  end
-
+  
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. 詳細画面へリダイレクト
+    redirect_to book_path(book.id)
   end
 
   def show
+    @book = Book.find(params[:id])#詳細画面で一件だけ本を持ってくるようにする記載
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'# 投稿一覧画面へリダイレクト 
+  end
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
