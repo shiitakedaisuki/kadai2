5d74a8311647552fc9db08d045591718573d2777
shiitakedaisuki <129662461+shiitakedaisuki@users.noreply.github.com>
2023-04-13 10:27:45 +0000

    Finish1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,12 +6,14 @@ class BooksController < ApplicationController
   end
   
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4. 詳細画面へリダイレクト
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] ="Book was successfully created."
+     redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+     render :index
+    end
   end
 
   def show
@@ -23,14 +25,19 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] ="Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
+    flash[:notice] ="Book was successfully destroyed."
     redirect_to '/books'# 投稿一覧画面へリダイレクト 
   end
   
